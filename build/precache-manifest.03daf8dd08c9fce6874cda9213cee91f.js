ae5338cfc4ee39714dfe6d4fa9ecba06e91a4aff
alexandershevchuk556 <alexandershevchuk556@gmail.com>
2021-11-29 21:26:46 +0300

    responsive update

---
+++ build/precache-manifest.03daf8dd08c9fce6874cda9213cee91f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44676a126f0989f60c5d260ba7d7cb4c",
+    "url": "//alexandershevchuk556.github.io/quotemachine/index.html"
+  },
+  {
+    "revision": "2ffb0e2c4b17c5f3ea65",
+    "url": "//alexandershevchuk556.github.io/quotemachine/static/css/main.5940d6da.chunk.css"
+  },
+  {
+    "revision": "99cd12066295ac044398",
+    "url": "//alexandershevchuk556.github.io/quotemachine/static/js/2.a134b155.chunk.js"
+  },
+  {
+    "revision": "2ffb0e2c4b17c5f3ea65",
+    "url": "//alexandershevchuk556.github.io/quotemachine/static/js/main.dbae4db1.chunk.js"
+  },
+  {
+    "revision": "2b9e91179c124b6f7c06",
+    "url": "//alexandershevchuk556.github.io/quotemachine/static/js/runtime-main.a55e0ae5.js"
+  }
+]);
