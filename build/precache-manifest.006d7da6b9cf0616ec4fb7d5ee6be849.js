9cac44e1ee68e49532caa48e52666b47944c0d0b
alexandershevchuk556 <alexandershevchuk556@gmail.com>
2021-11-29 20:07:57 +0300

    github pages

---
+++ build/precache-manifest.006d7da6b9cf0616ec4fb7d5ee6be849.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1fcf771ba1673a73e6395d36b20ba6",
+    "url": "//alexandershevchuk556.github.io/quotemachine/index.html"
+  },
+  {
+    "revision": "fd676c24de727c1d7e24",
+    "url": "//alexandershevchuk556.github.io/quotemachine/static/css/main.371b4704.chunk.css"
+  },
+  {
+    "revision": "483e7951e1aab5963597",
+    "url": "//alexandershevchuk556.github.io/quotemachine/static/js/2.ffb32053.chunk.js"
+  },
+  {
+    "revision": "fd676c24de727c1d7e24",
+    "url": "//alexandershevchuk556.github.io/quotemachine/static/js/main.5a23f5d8.chunk.js"
+  },
+  {
+    "revision": "8e71f7e66a986692394b",
+    "url": "//alexandershevchuk556.github.io/quotemachine/static/js/runtime-main.20b81527.js"
+  }
+]);
